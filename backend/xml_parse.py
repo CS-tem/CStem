5fc274cb3a854bf454dc88d6e274c6074e23336c
devansh-dvj <devansh.dvj@gmail.com>
2022-04-01 04:41:04 +0530

    Added print_xml_tree for inproceedings+None

--- backend/xml_parse.py
+++ backend/xml_parse.py
@@ -2,18 +2,18 @@ from lxml import etree
 
 
 publ_records = [
-    'article',
+    # 'article',
     'inproceedings',
-    'proceedings',
-    'book',
-    'incollection',
-    'phdthesis',
-    'mastersthesis',
-    'www'
+    # 'proceedings',
+    # 'book',
+    # 'incollection',
+    # 'phdthesis',
+    # 'mastersthesis',
+    # 'www'
 ]
 
 publtypes = [
-    None,
+    # None,
     'informal',
     'withdrawn',
     'data',
@@ -28,6 +28,14 @@ publtypes = [
     'group'
 ]
 
+
+def print_xml_tree(tree):
+    """
+    Prints the XML tree to the console.
+    """
+    print(etree.tostring(tree, pretty_print=True).decode('utf-8'))
+
+
 if __name__ == '__main__':
     context = etree.iterparse("data/dblp.xml", events=('start', 'end'), load_dtd=True)
 
@@ -37,8 +45,12 @@ if __name__ == '__main__':
             if elem.tag in publ_records:
                 if elem.attrib.get('publtype') not in publtypes:
                     count += 1
+                    if count % 1000 == 0:
+                        print(count)
                     print(elem.attrib)
+                    print(elem.xpath('string()'))
                     print(len(elem.getchildren()))
+                    print_xml_tree(elem)
         if event == 'end':
             elem.clear()
         if count == 100:
