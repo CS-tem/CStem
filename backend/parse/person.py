0265466b8ca5ada3a3fed65eaf01e9769732c020
devansh-dvj <devansh.dvj@gmail.com>
2022-04-02 05:07:36 +0530

    Updated person and inproceeding samples

--- backend/parse/person.py
+++ backend/parse/person.py
@@ -1,10 +1,40 @@
-import utils
-from config import *
 from lxml import etree
+
+import config
+import utils
+
+
+def samplePerson(context, output_file, n_samples=-1):
+
+    with open(output_file, 'w+') as f:
+
+        f.write('<?xml version="1.0" encoding="ISO-8859-1"?>\n')
+        f.write('<!DOCTYPE dblp SYSTEM "dblp.dtd">\n\n')
+        f.write('<dblp>\n\n')
+
+        count = 0
+        for event, elem in context:
+            if event == 'end':
+                if elem.tag == 'www' and 'homepages' in elem.attrib.get('key'):
+                    f.write(etree.tostring(elem, pretty_print=True).decode('utf-8'))
+                    f.write('\n')
+                    count += 1
+                    if count == n_samples:
+                        break
+                elem.clear()
+
+        f.write('\n</dblp>\n')
+
+    return count
+
 
 if __name__ == '__main__':
 
-    context = etree.iterparse(f'{PATH_DATA}/dblp.xml', events=('start', 'end'), load_dtd=True)
+    context = etree.iterparse(f'{config.PATH_DATA}/dblp.xml', events=('end',), load_dtd=True)
+
+    count = samplePerson(context, f'{config.PATH_DATA}/person.xml', -1)
+    print(count)
+    exit(0)
 
     count = 0
     for event, elem in context:
--- backend/parse/publ.py
+++ backend/parse/publ.py
@@ -1,40 +1,46 @@
-import utils
-from config import *
 from lxml import etree
 
-def sampleInproceedings(context, output_file, n_samples = -1):
+import config
+import utils
+
+
+def sampleInproceedings(context, output_file, n_samples=-1):
 
     with open(output_file, 'w+') as f:
 
-        f.write('<?xml version="1.0" encoding="ISO-8859-1"?>')
-        f.write('<!DOCTYPE dblp SYSTEM "dblp.dtd">')
-        f.write('<dblp>')
+        f.write('<?xml version="1.0" encoding="ISO-8859-1"?>\n')
+        f.write('<!DOCTYPE dblp SYSTEM "dblp.dtd">\n\n')
+        f.write('<dblp>\n\n')
 
         count = 0
         for event, elem in context:
-            if event == 'end' and elem.tag == 'inproceedings' and elem.attrib.get('publtype') is None:
-                f.write(etree.tostring(elem, pretty_print = True).decode('utf-8'))
-                f.write("\n")
-                count += 1
-                if count == n_samples:
-                    break
+            if event == 'end':
+                if elem.tag == 'inproceedings' and elem.attrib.get('publtype') is None:
+                    f.write(etree.tostring(elem, pretty_print=True).decode('utf-8'))
+                    f.write('\n')
+                    count += 1
+                    if count == n_samples:
+                        break
                 elem.clear()
 
-        f.write('</dblp>')
+        f.write('\n</dblp>\n')
+
+    return count
 
 
 if __name__ == '__main__':
 
-    context = etree.iterparse(f'{PATH_DATA}/dblp.xml', events = ('end',), load_dtd = True)
+    context = etree.iterparse(f'{config.PATH_DATA}/dblp.xml', events=('end',), load_dtd=True)
 
-    sampleInproceedings(context, f'{PATH_DATA}/inproceedings.xml', 10)
+    count = sampleInproceedings(context, f'{config.PATH_DATA}/inproceedings.xml', -1)
+    print(count)
     exit(0)
 
     count = 0
     for event, elem in context:
         if event == 'start':
-            if elem.tag in PUBL_RECORDS:
-                if elem.attrib.get('publtype') in PUBL_TYPES:
+            if elem.tag in config.PUBL_RECORDS:
+                if elem.attrib.get('publtype') in config.PUBL_TYPES:
                     count += 1
                     if count % 1000 == 0:
                         print(count)
@@ -47,11 +53,12 @@ if __name__ == '__main__':
         if count == 100:
             break
     print(count)
+    exit(0)
 
-    # for event, elem in context:
-    #     if event == 'start':
-    #         if elem.tag in PUBL_RECORDS:
-    #             PUBL_STATS[elem.tag][elem.attrib.get('publtype')] += 1
-    #     if event == 'end':
-    #         elem.clear()
-    # print(PUBL_STATS)
+    for event, elem in context:
+        if event == 'start':
+            if elem.tag in config.PUBL_RECORDS:
+                config.PUBL_STATS[elem.tag][elem.attrib.get('publtype')] += 1
+        if event == 'end':
+            elem.clear()
+    print(config.PUBL_STATS)
