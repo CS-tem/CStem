5e7f7e993782aef86c92957677da924e650b0de0
Harshit Varma <varmaharshit2@gmail.com>
2022-04-01 14:31:41 +0530

    fixed inproceedings issue

--- backend/parse/publ.py
+++ backend/parse/publ.py
@@ -2,27 +2,32 @@ import utils
 from config import *
 from lxml import etree
 
-def sampleInproceedings(context, output_file, max=-1):
-    with open(output_file, 'w') as f:
+def sampleInproceedings(context, output_file, n_samples = -1):
+
+    with open(output_file, 'w+') as f:
+
         f.write('<?xml version="1.0" encoding="ISO-8859-1"?>')
         f.write('<!DOCTYPE dblp SYSTEM "dblp.dtd">')
         f.write('<dblp>')
+
         count = 0
         for event, elem in context:
-            if event == 'end':
-                if elem.tag == 'inproceedings' and elem.attrib.get('publtype') is None:
-                    f.write(etree.tostring(elem, pretty_print=True).decode('utf-8'))
-                    count += 1
-                    if count == max:
-                        break
+            if event == 'end' and elem.tag == 'inproceedings' and elem.attrib.get('publtype') is None:
+                f.write(etree.tostring(elem, pretty_print = True).decode('utf-8'))
+                f.write("\n")
+                count += 1
+                if count == n_samples:
+                    break
                 elem.clear()
+
         f.write('</dblp>')
 
 
 if __name__ == '__main__':
-    context = etree.iterparse(f'{PATH_DATA}/dblp.xml', events=('start', 'end'), load_dtd=True)
 
-    sampleInproceedings(context, f'{PATH_DATA}/inproceedings.xml')
+    context = etree.iterparse(f'{PATH_DATA}/dblp.xml', events = ('end',), load_dtd = True)
+
+    sampleInproceedings(context, f'{PATH_DATA}/inproceedings.xml', 10)
     exit(0)
 
     count = 0
