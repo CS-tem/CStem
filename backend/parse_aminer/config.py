8c2f9b6a223db476be237525b45d4ea44d2e28fa
devansh-dvj <devansh.dvj@gmail.com>
2022-04-11 03:46:11 +0530

    aminer: Added id map of CV venue

--- backend/parse_aminer/config.py
+++ backend/parse_aminer/config.py
@@ -26,11 +26,12 @@ ESSENTIAL_KEYS = (
     'year'
 )
 
-VENUE_ID_MAP = [
-    ('53a727c520f7420be8b9ba3d', 'European Conference on Computer Vision'),
-    ('5390797220f770854f5bb95f', 'International Journal of Computer Vision'),
-    ('53a7266b20f7420be8b75e74', 'International Conference on Computer Vision Theory and Applications'),
-    ('53a72a0320f7420be8bed092', 'International Conference on Computer Vision Systems'),
-    ('53a72d7620f7420be8c6646b', 'International Conference on Computer Vision'),
-    ('54824c66582fc50b5e008eb0', 'Asian Conference on Computer Vision'),
-]
+VENUE_ID_MAP = {
+    '53a727c520f7420be8b9ba3d': 'European Conference on Computer Vision',
+    # '5390797220f770854f5bb95f': 'International Journal of Computer Vision',
+    # '53a7266b20f7420be8b75e74': 'International Conference on Computer Vision Theory and Applications',
+    # '53a72a0320f7420be8bed092': 'International Conference on Computer Vision Systems',
+    '53a72d7620f7420be8c6646b': 'International Conference on Computer Vision',
+    # '54824c66582fc50b5e008eb0': 'Asian Conference on Computer Vision',
+    '53a7256420f7420be8b4e0aa': 'Computer Vision and Pattern Recognition',
+}
