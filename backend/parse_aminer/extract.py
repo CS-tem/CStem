7d596b1afaf53f947f52bc070bbef27ffd92eeca
devansh-dvj <devansh.dvj@gmail.com>
2022-04-11 03:45:42 +0530

    aminer: Fixed minor json parsing bug in extract

--- backend/parse_aminer/extract.py
+++ backend/parse_aminer/extract.py
@@ -16,22 +16,27 @@ if __name__ == '__main__':
         parse_events = ijson.parse(f)
 
         count = 0
+        first = True
         for obj in ijson.items(parse_events, 'item'):
             count += 1
             if count % 10000 == 0:
                 print(count)
 
             if obj.get('venue') is not None:
+                if not first:
+                    out.write(',\n')
+                else:
+                    first = False
+
                 extracted = {}
                 for key in config.ESSENTIAL_KEYS:
                     extracted[key] = obj.get(key, None)
                 out.write(pretty(extracted))
 
-                out.write(',\n')
             # if count == 100:
             #     break
 
         print(count)
 
-        out.write(']\n')
+        out.write('\n]\n')
         out.close()
