9c286a8f08c23a7fe55d73dae59d0695109f4794
Adithya Bhaskar <zer0.h6ck@gmail.com>
2022-04-11 17:25:40 +0530

    Added FastAPI code for cypher

---
+++ backend/neoquery.py
@@ -0,0 +1,25 @@
+from neo4j import GraphDatabase
+import logging
+from neo4j.exceptions import ServiceUnavailable
+
+class Graph:
+    def __init__(self, uri='bolt://localhost:7687/', user='neo4j', password='neo4j'):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def parse_result(self, result):
+        return [record.data() for record in result]
+    
+    def neo4j_query(self, query : str):
+        with self.driver.session() as session:
+            return self.parse_result(session.run(query))
+
+    def close(self):
+        self.driver.close()
+
+if __name__ == '__main__':
+    """
+    Neo4j testing code.
+    """
+    graph = Graph(user='neo4j', password='adshaila')
+    query = 'MATCH (i: Institute) return i;'
+    print(graph.neo4j_query(query))
--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from neoquery import Graph
+from typing import Optional
 
 app = FastAPI()
 origins = ['*']
@@ -12,7 +13,83 @@ app.add_middleware(
     allow_headers=['*']
 )
 
+neo_db = Graph(uri='bolt://localhost:7687/', user='neo4j', password='adshaila')
 
-@ app.get('/')
+@app.get('/')
 def test():
     return {'status': 200, 'message': 'test works'}
+
+@app.get('/institutes/{institute_id}')
+def get_institutes(institute_id : int):
+    query = 'MATCH (i: Institute {{id : {}}}) RETURN i;'.format(institute_id)
+    result = neo_db.neo4j_query(query)
+    return [entry['i'] for entry in result]
+
+@app.get('/institute-pubs/{institute_id}')
+def get_institute_pubs(institute_id : int):
+    query = """MATCH (i : Institute{{id: {}}})-[:InstituteMember]->
+                (j : Author)-[:AuthorArticle]->(k: Article)
+                RETURN  i AS institute, COUNT(k.id) AS n_pubs, 
+                k.year as year ORDER BY year""".format(institute_id)
+    result = neo_db.neo4j_query(query)
+    return result
+
+@app.get('/institute-citations/{institute_id}')
+def get_institute_citations(institute_id : int):
+    query = """
+    MATCH (i : Institute{{id: {}}})-[:InstituteMember]->(j : Author)-[:AuthorArticle]->(k: Article) RETURN 
+    i AS institute, SUM(k.n_citations) AS n_citations, k.year as year ORDER BY year; """.format(institute_id)
+    result = neo_db.neo4j_query(query)
+    
+    return result
+
+@app.get('/institutes/')
+def get_institutes():
+    query = 'MATCH (i: Institute) RETURN i;'
+    result = neo_db.neo4j_query(query)
+    return [entry['i'] for entry in result]
+
+@app.get('/authors/{author_id}')
+def get_authors(author_id : int):
+    query = 'MATCH (i:Author{{id : {}}})-[:AuthorArticle]->(j)RETURN i,j;'.format(author_id)
+    result = neo_db.neo4j_query(query)
+    return result
+
+@app.get('/author-pubs/{author_id}')
+def get_author_pubs(author_id : int):
+    query = """MATCH (i : Author{{id: {}}})-[:AuthorArticle]->(j : Article)
+        RETURN i AS author, count(j.id) AS n_pubs, j.year as year ORDER BY year;""".format(author_id)
+    result = neo_db.neo4j_query(query)
+    return result
+
+@app.get('/author-citations/{author_id}')
+def get_author_citations(author_id : int):
+    query = """
+    MATCH(j : Author{{id: {}}})-[:AuthorArticle]->(k: Article)
+    RETURN j AS author, SUM(k.n_citations) AS n_citations, k.year as year
+    ORDER BY year;""".format(author_id)
+    result = neo_db.neo4j_query(query)
+    
+    return result
+
+@app.get('/author-pubs-per-topic/{author_id}')
+def get_author_pubs_per_topic(author_id : int):
+    query = """MATCH(j : Author{{id: {}}})-[:AuthorArticle]->
+            (k: Article)<-[:ArticleTopic]-(i:Topic)
+            RETURN j AS author, COUNT(k.id) AS n_pubs, i AS topic
+            ORDER BY topic""".format(author_id)
+    result = neo_db.neo4j_query(query)
+    return result
+
+@app.get('/author-colab/{author_id}')
+def get_author_colabs(author_id : int):
+    query = """MATCH (j : Author{{id : {}}})-[k:Coauthor]->(i)
+            RETURN j AS author1, i AS author2, k.n_colab as n_colab;""".format(author_id)
+    result = neo_db.neo4j_query(query)
+    return result
+
+@app.get('/authors/')
+def get_authors():
+    query = 'MATCH (i: Author) RETURN i;'
+    result = neo_db.neo4j_query(query)
+    return [entry['i'] for entry in result]
