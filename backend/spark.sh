5f05fa8cda43f2f8df36dabbfc40321cf1a7e274
devansh-dvj <devansh.dvj@gmail.com>
2022-04-12 15:53:17 +0530

    Added composite spark script

---
+++ backend/spark.sh
@@ -0,0 +1,11 @@
+cd "$(dirname "$0")"
+
+mkdir -p data/csv/post_spark
+cp data/csv/pre_spark/* data/csv/post_spark
+
+python fill_metrics.py -o article;
+python fill_metrics.py -o author;
+python fill_metrics.py -o institute;
+python fill_metrics.py -o author_topic;
+python fill_metrics.py -o topic;
+python fill_metrics.py -o venue;
