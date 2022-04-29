6bc59ad102eb1d452a916785eaa728275572d009
Harshit Varma <varmaharshit2@gmail.com>
2022-04-29 16:47:10 +0530

    citation graph done

--- frontend/src/app/article/article.component.ts
+++ frontend/src/app/article/article.component.ts
@@ -133,14 +133,17 @@ export class ArticleComponent implements OnInit {
               nodes_set.add(path.nodes[i].id);
               this.nodes_list.push({
                 id: path.nodes[i].id,
-                label: path.nodes[i].title
+                label: path.nodes[i].id,
+                title: path.nodes[i].title,
+                color: 'green'
               });
             }
             if (!nodes_set.has(path.nodes[i+1].id)) {
               nodes_set.add(path.nodes[i+1].id);
               this.nodes_list.push({
                 id: path.nodes[i+1].id,
-                label: path.nodes[i+1].title
+                label: path.nodes[i+1].id,
+                title: path.nodes[i+1].title
               });
             }
             // Use the concatenated string as the 'key' to check if already done
@@ -166,23 +169,29 @@ export class ArticleComponent implements OnInit {
         const container = this.citations;
 
         this.networkInstance = new Network(container.nativeElement, data, {
+          
+          autoResize: true,
+
           height: '100%',
-          width: '100%',
+          width: '60%',
+
           nodes: {
-            shape: 'hexagon',
+            shape: 'ellipse',
             font: {
-              color: 'white',
+              color: '#000000',
+              size: 14
             },
           },
+
           edges: {
-            smooth: true,
             arrows: {
               to: {
                 enabled: true,
-                type: 'vee',
+                // type: 'vee',
               },
             },
           },
+
         });
 
       })
